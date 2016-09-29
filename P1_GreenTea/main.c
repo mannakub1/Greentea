b1f81a5e60f44c63dad9d51df9a6c65c58a17fe6
Nanthipath <omliler_man@hotmail.com>
2016-09-29 09:22:47 +0700

    first commit

---
+++ P1_GreenTea/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  P1_GreenTea
+//
+//  Created by SunDay on 9/29/2559 BE.
+//  Copyright © 2559 Silpakorn. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("Hello, World!\n");
+    return 0;
+}
